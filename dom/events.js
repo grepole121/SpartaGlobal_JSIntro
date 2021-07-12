5f2b3d10a431d44a7543dc9845d6bc941f0445bb
George Repole <grepole121@users.noreply.github.com>
2021-07-12 20:51:11 +0100

    Finished most of calculator (excluding % and +/-)

--- dom/events.js
+++ dom/events.js
@@ -1,3 +1,13 @@
+function reset() {
+  firstNumber = null;
+  operator = null;
+  secondNumber = null;
+  result = null;
+  screen.innerHTML = "";
+}
+
+reset();
+
 var firstNumber = null;
 var operator = null;
 var secondNumber = null;
@@ -6,31 +16,62 @@ var result = null;
 var buttons = document.getElementsByClassName("button");
 var screen = document.getElementById("screen");
 
-function calculateResult(){
-    if(secondNumber != null){
-        switch(operator){
-            case "+":
-                result = parseInt(firstNumber) + parseInt(secondNumber);
-                break;
-        }
-        screen.innerText = result;
+function calculateResult() {
+  if (secondNumber != null) {
+    switch (operator) {
+      case "+":
+        result = parseFloat(firstNumber) + parseFloat(secondNumber);
+        break;
+      case "*":
+        result = parseFloat(firstNumber) * parseFloat(secondNumber);
+        break;
+      case "-":
+        result = parseFloat(firstNumber) - parseFloat(secondNumber);
+        break;
+      case "÷":
+        result = parseFloat(firstNumber) / parseFloat(secondNumber);
+        break;
     }
+    screen.innerText = Math.round(result * 100) / 100;
+    firstNumber = result;
+    operator = null;
+    secondNumber = null;
+  }
 }
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function () {
-    screen.innerHTML = buttons[i].innerHTML;
-
+    currentButton = buttons[i].innerText;
     if (!firstNumber) {
-      firstNumber = buttons[i].innerText;
+      if (!isNaN(currentButton) || currentButton == ".") {
+        firstNumber = currentButton;
+        screen.innerHTML += buttons[i].innerHTML;
+      }
     } else if (!operator) {
-      operator = buttons[i].innerText;
+      if (currentButton.match(/[-\*\+÷]/g)) {
+        operator = currentButton;
+        screen.innerHTML += buttons[i].innerHTML;
+      } else if (!isNaN(currentButton) || currentButton == ".") {
+        screen.innerHTML += buttons[i].innerHTML;
+        firstNumber += currentButton;
+      }
     } else if (!secondNumber) {
-      secondNumber = buttons[i].innerText;
+      if (!isNaN(currentButton)) {
+        secondNumber = currentButton;
+        screen.innerHTML += buttons[i].innerHTML;
+      }
+    } else if (secondNumber != null) {
+      if (!isNaN(currentButton) || currentButton == ".") {
+        secondNumber += currentButton;
+        screen.innerHTML += buttons[i].innerHTML;
+      }
     }
   });
 
   if (buttons[i].innerText == "=") {
-    buttons[i].addEventListener("click", calculateResult)
+    buttons[i].addEventListener("click", calculateResult);
+  }
+  if (buttons[i].innerText == "AC") {
+    buttons[i].addEventListener("click", reset);
   }
 }
